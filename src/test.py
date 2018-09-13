3936097cf039a508f50697f93394d5663db6fda0
biseshbhattarai <aashishbhattarai49@gmail.com>
2018-09-13 21:15:19 +0545

    Initial commit

---
+++ src/test.py
@@ -0,0 +1,7 @@
+import sentiment_mod as mod 
+
+print(mod.sentiment('It was an awful movie literally didnt like it , i hate it terribly so rubbish idiotic movie rated 0/ 10'))
+print(mod.sentiment('The past cannot be changed. The future is yet in your power.'))
+print(mod.sentiment('It does not matter how slowly you go as long as you do not stop. '))
+print(mod.sentiment("This film served as great entertainment with its colorful cast and numerous plot twists. Nolan used actors that had either appeared in previous Batman films or in his blockbuster hit Inception, and all of them shone in their respective roles: Tom Hardy was almost unrecognizable in his Bane costume, while Joseph Gordon-Levitt and Marion Cotillard were both excellent—and obviously comfortable with Nolan’s directing style and the film’s dramatic tone."))
+print(mod.sentiment('It was an awful movie literally didnt like it , i hate it terribly so rubbish idiotic movie rated 0/ 10'))
